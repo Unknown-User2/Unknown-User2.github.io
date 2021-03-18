46071648c5bb3c577e4c5a6315f34742d12f6a39
Unknown-User2 <benjaminjneudorf@gmail.com>
2021-03-18 17:42:55 -0500

    working on a header and footer

--- js/homepage/darkmodebutton.js
+++
@@ -1,8 +0,0 @@
-// Select the button
-const darkToggle = document.querySelector('#darkToggle');
-
-// Listen for a click on the button
-darkToggle.addEventListener('click', function() {
-  // Then toggle (add/remove) the .dark-theme class to the body
-  document.body.classList.toggle('darkMode');  
-})
