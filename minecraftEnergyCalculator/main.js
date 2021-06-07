2d7c2b4043379f9d3e5002472061bc9256a78dff
Unknown-User2 <benjaminjneudorf@gmail.com>
2021-06-06 19:06:39 -0500

    finished all the rf based units

--- minecraftEnergyCalculator/main.js
+++ minecraftEnergyCalculator/main.js
@@ -16,13 +16,13 @@ function isNumber(key) {
 // is called when a key goes up in the input 
 function convert() {
 	console.info("convert()")
-	inputVal = input.value
+	getVals()
 	check()
 }
 
 function check() {
 	switch(side0Val + "|" +  side1Val) {
-		// rf to ...
+		// RF to ...
 		case "rf|rf":
 			sameConvert()
 			break
@@ -44,6 +44,72 @@ function check() {
 		case "rf|mj":
 			output.value = inputVal / 10
 			break
+		// kRF to ...
+		case "krf|rf":
+			output.value = bigRFToRF(side0Val, inputVal)
+			break
+		case "krf|krf":
+			output.value = sameConvert()
+			break
+		case "krf|mrf":
+			output.value = inputVal / 1000
+			break
+		case "krf|grf":
+			output.value = inputVal / 1000000
+			break
+		case "krf|eu":
+			output.value = bigRFToRF(side0Val, inputVal) / 4
+			break
+		case "krf|j":
+			output.value = bigRFToRF(side0Val, inputVal) * 2.5
+			break
+		case "krf|mj":
+			output.value = bigRFToRF(side0Val, inputVal) / 10
+			break
+		// mRF to ..
+		case "mrf|rf":
+			output.value = bigRFToRF(side0Val, inputVal)
+			break
+		case "mrf|krf":
+			output.value = inputVal * 1000
+			break
+		case "mrf|mrf":
+			output.value = sameConvert()
+			break
+		case "mrf|grf":
+			output.value = inputVal / 1000000
+			break
+		case "mrf|eu":
+			output.value = bigRFToRF(side0Val, inputVal) / 4
+			break
+		case "mrf|j":
+			output.value = bigRFToRF(side0Val, inputVal) * 2.5
+			break
+		case "mrf|mj":
+			output.value = bigRFToRF(side0Val, inputVal) / 10
+			break
+		// gRF to ...
+		case "grf|rf":
+			output.value =  bigRFToRF(side0Val, inputVal)	
+			break
+		case "grf|krf":
+			output.value = inputVal * 1000000
+			break
+		case "grf|mrf":
+			output.value = inputVal * 1000
+			break
+		case "grf|grf":
+			output.value = sameConvert()
+			break
+		case "grf|eu":
+			output.value = bigRFToRF(side0Val, inputVal) / 4
+			break
+		case "grf|j":
+			output.value = bigRFToRF(side0Val, inputVal) * 2.5
+			break
+		case "grf|mj":
+			output.value = bigRFToRF(side0Val, inputVal) / 10
+			break
 	}
 }
 
@@ -52,7 +118,27 @@ function sameConvert() {
 	output.value = inputVal
 }
 
-function getSide() {
+// converts kRF, mRF or gRF to RF
+function bigRFToRF(unit, val) {
+	switch(unit) {
+		case "krf":
+			return val * 1000
+			break
+		case "mrf":
+			return val * 1000000
+			break
+		case "grf":
+			return val * 1000000000
+			break
+		default:
+			console.error("Invaild option for convertion for function sameConvert()")
+			return "Invaild option for convertion for function sameConvert()"
+			break
+	}
+}
+
+function getVals() {
 	side0Val = side0.value
 	side1Val = side1.value
+	inputVal = input.value
 }
